851724369344951f82fb12d85046815c8b677157
Alex Perez <alexjperez@gmail.com>
2015-03-18 16:24:13 -0700

    Added ability to output ROC curve plots

--- segstats.py
+++ segstats.py
@@ -17,15 +17,38 @@ def usage(errstr):
     print ""
     exit(1)
 
+def writeROCplot(FPR, TPR, fnamein, fnameout, opts):
+    plt.plot(FPR, TPR, linewidth = 2, label = "ROC")
+    if opts.randline:
+        x = [0.0, 1.0]
+        plt.plot(x, x, linestyle = "dashed", color = "red", linewidth = 2,
+                 label = "Random")
+    plt.xlabel("False Positive Rate", fontsize = 14)
+    plt.ylabel("True Positive Rate", fontsize = 14)
+    plt.title("ROC Curve - {0}".format(fnamein, fontsize = 14))
+    plt.xlim(0.0, 1.0)
+    plt.ylim(0.0, 1.0)
+    plt.legend(fontsize = 10, loc = "best")
+    plt.tight_layout()
+    plt.savefig(fnameout)
+    plt.close()
+
 if __name__ == "__main__":
     p = OptionParser(usage = "%prog [options] probmap.png groundtruth.png", 
                      epilog =
                      "Example: %s probmap.png groundtruth.png"
                      % os.path.basename(argv[0]))    
  
-    p.add_option("-o", dest = "path_out", metavar = "PATH",
-                 help = "Output path to save stats to. {DEFAULT = Current "
+    p.add_option("--output", dest = "path_out", metavar = "PATH",
+                 help = "Output path to save stats to. (DEFAULT = Current "
                         "working directory).")
+
+    p.add_option("--noroc", action = "store_true", dest = "noroc",
+                 help = "Turns off writing of ROC plots.")
+
+    p.add_option("--randomline", action = "store_true", dest = "randline",
+                 help = "Adds a line equivalent to making a random choice "
+                        "to all ROC plots.")
 
     (opts, args) = p.parse_args()
 
@@ -46,6 +69,9 @@ if __name__ == "__main__":
               "output path {0}".format(opts.path_out))
     os.makedirs(path_out)
 
+    if not opts.noroc:
+        os.makedirs(os.path.join(path_out, "roc"))
+
     # Check if input arguments are directories or single image files
     if os.path.isdir(pm_in):
         pm_files = sorted(glob.glob("{0}/*".format(pm_in)))
@@ -59,34 +85,59 @@ if __name__ == "__main__":
     if len(pm_files) != len(gt_files):
         usage("The number of probability map images does not equal the "
               "number of ground truth images.")
+    nfiles = len(pm_files)
 
-    # Loop
-    for i in range(0, len(pm_files)):
-        pmi = misc.imread(pm_files[0])
-        gti = misc.imread(gt_files[0])
+    # Loop 
+    FParray = np.zeros([nfiles, 256], dtype = "f")
+    FNarray = np.zeros([nfiles, 256], dtype = "f")
+    TParray = np.zeros([nfiles, 256], dtype = "f")
+    TNarray = np.zeros([nfiles, 256], dtype = "f")
+    for i in range(0, nfiles):
+        TPRlist = []
+        FPRlist = []
+        print "Analyzing file {0}...".format(pm_files[i])
+        bname = os.path.basename(pm_files[i])
+        pmi = misc.imread(pm_files[i])
+        gti = misc.imread(gt_files[i])
         if (pmi.shape[0] != gti.shape[0]) or (pmi.shape[1] != gti.shape[1]):
             usage("The images {0} and {1} do not have the same "
                   "dimensions".format(pm_files[i], gt_files[i]))
         for j in range(0, 256):
+            # Threshold the image at the given intensity level
             threshi = np.where(pmi > j, 1, 0)
+            
+            # Compute the confusion matrix
             FP = np.float(np.sum((threshi == 1) & (gti == 0)))
             FN = np.float(np.sum((threshi == 0) & (gti == 1)))
             TP = np.float(np.sum((threshi == 1) & (gti == 1)))
             TN = np.float(np.sum((threshi == 0) & (gti == 0)))
-            
+           
             FPR = FP/(FP+TN)
-            FNR = FN/(FN+TP)
             TPR = TP/(TP+FN)
+            FNR = FN/(FN+TP)
             TNR = TN/(FP+TN)
- 
-            print "{0} {1} {2}".format(j, FPR, TPR)
 
+            FParray[i, j] = FP
+            FNarray[i, j] = FN
+            TParray[i, j] = TP
+            TNarray[i, j] = TN
+            
+            TPRlist.append(TPR)
+            FPRlist.append(FPR)
 
+        if not opts.noroc:
+            fnameout = os.path.join(path_out, "roc", "{0}.png".format(
+                                    bname.split(".")[0]))
+            writeROCplot(FPRlist, TPRlist, bname, fnameout, opts)
 
-
-
-
-
-
-    
+    FPsum = np.sum(FParray, 0)
+    FNsum = np.sum(FNarray, 0)
+    TPsum = np.sum(TParray, 0)
+    TNsum = np.sum(TNarray, 0)
+    if not opts.noroc:
+        with np.errstate(invalid = "ignore"):
+            FPRsum = FPsum/(FPsum+TNsum)
+            TPRsum = TPsum/(TPsum+FNsum)
+        fnameout = os.path.join(path_out, "roc", "sum.png")
+        writeROCplot(FPRsum, TPRsum, "sum", fnameout, opts)
 
