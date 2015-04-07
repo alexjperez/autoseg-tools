7a025027c0774c5535ec21436f1397ebc8b2f52b
Alex Perez <alexjperez@gmail.com>
2015-04-06 17:32:32 -0700

    Updated to run on Amira 6.0

--- mito_skeleton.hx
+++ mito_skeleton.hx
@@ -1,76 +1,85 @@
 # Amira Script
 
-proc getvsa {file base} {
-    # Create surface from the MRC stack of binary labels
-    create {HxGMC} {SurfaceGen}
-    SurfaceGen data connect $file
-    SurfaceGen smoothing setValue 3
-    SurfaceGen options setValue 0 1
-    SurfaceGen options setValue 1 1
-    SurfaceGen border setValue 0 1
-    SurfaceGen action snap
-    SurfaceGen fire
+proc getvsa {file} {
+    # Create a label stack from the binary MRC stack
+    create auto_threshold "Auto Thresholding"
+    "Auto Thresholding" inputImage connect $file
+    "Auto Thresholding" doIt hit
+    "Auto Thresholding" fire
+
+    # Create surface from the label stack
+    create HxGMC "Generate Surface"
+    "Generate Surface" data connect $file.labels
+    "GenerateSurface" smoothing setValue 3 #Unconstrained smoothing
+    "GenerateSurface" options setValue 0 1
+    "GenerateSurface" options setValue 1 1
+    "GenerateSurface" border setValue 0 1
+    "Generate Surface" action snap
+    "Generate Surface" fire
 
     # Remesh surface
-    create {HxRemeshSurface} {RemeshSurface}
-    RemeshSurface data connect $base.surf
-    RemeshSurface objective setValue 1
-    RemeshSurface desiredSize setValue 2 50
-    RemeshSurface interpolateOrigSurface setValue 0
-    RemeshSurface remesh snap
-    RemeshSurface fire
+    create HxRemeshSurface "Remesh Surface"
+    "Remesh Surface" data connect $file.surf
+    "Remesh Surface" objective setValue 1
+    "Remesh Surface" desiredSize setValue 2 50
+    "Remesh Surface" interpolateOrigSurface setValue 0
+    "Remesh Surface" remesh snap
+    "Remesh Surface" fire
     
     # Output statistics (volume, surface area)
-    create {HxSurfaceArea} {SurfaceArea}
-    SurfaceArea data connect $base.remeshed
-    SurfaceArea action snap
-    SurfaceArea fire
+    create HxSurfaceArea "Surface Area"
+    "Surface Area" data connect $file.remeshed
+    "Surface Area" action snap
+    "Surface Area" fire
 }
 
-proc getskl {file base} {
+proc getskl {file} {
     # Create CenterlineTree skeleton object. Make sure the parameters are set 
     # to their default values.
-    create {HxTEASAR} {CenterlineTree}
-    CenterlineTree data connect $file
-    CenterlineTree tubesParams setValue 0 2.5
-    CenterlineTree tubesParams setValue 1 4
-    CenterlineTree numParts setValue -1
-    CenterlineTree options setValue 0 1
-    CenterlineTree doIt snap
-    CenterlineTree fire
+    create HxTEASAR "Centerline Tree"
+    "Centerline Tree" data connect $file
+    "Centerline Tree" tubesParams setValue 0 2.5
+    "Centerline Tree" tubesParams setValue 1 4
+    "Centerline Tree" numParts setValue -1
+    "Centerline Tree" options setValue 0 1
+    "Centerline Tree" doIt snap
+    "Centerline Tree" fire
     
     # Smooth the skeleton. Create a Smooth module and connect it to the 
     # skeleton. Set the smoothing parameters to 0.7 and 0.2. These seem to 
     # yield the best results for isotropic data.
-    create {HxSmoothLine} {Smooth}
-    Smooth lineSet connect $base.SpatialGraph
-    Smooth coefficients setValues 0.7 0.2
-    Smooth numberOfIterations setValue 10
-    Smooth doIt snap
-    Smooth fire
+    create HxSmoothLine "Smooth"
+    "Smooth" lineSet connect $file.Spatial-Graph
+    "Smooth" coefficients setValues 0.7 0.2
+    "Smooth" numberOfIterations setValue 10
+    "Smooth" doIt snap
+    "Smooth" fire
    
     # Output statistics (branch length and number of nodes)
-    create {HxSpatialGraphStats} {SpatialGraphStatistics}
-    SpatialGraphStatistics data connect SmoothTree.spatialgraph
-    SpatialGraphStatistics doIt snap
-    SpatialGraphStatistics fire
+    create HxSpatialGraphStats "Spatial Graph Statistics"
+    "Spatial Graph Statistics" data connect SmoothTree.spatialgraph
+    "Spatial Graph Statistics" doIt snap
+    "Spatial Graph Statistics" fire
 }
 
-proc saveimg {file base} {
+proc saveimg {file} {
     # Make the surface transparent
-    SurfaceView drawStyle setValue 4
-    SurfaceView fire
+    create HxDisplaySurface "Surface View"
+    "Surface View" data connect $file.remeshed
+    "Surface View" drawStyle setValue 4
+    "Surface View" fire
 
     # Make the skeleton red and enlarge the node spheres a little
-    create {HxSpatialGraphView} {SpatialGraphView}
-    SpatialGraphView data connect SmoothTree.spatialgraph
-    SpatialGraphView segmentColor setColor 0 1 0 0
-    SpatialGraphView fire
-    SpatialGraphView nodeScaleFactor setValue 200	
-    SpatialGraphView fire
+    create HxSpatialGraphView "Spatial Graph View"
+    "Spatial Graph View" data connect SmoothTree.spatialgraph
+    "Spatial Graph View" segmentColor setColor 0 1 0 0
+    "Spatial Graph View" fire
+    "Spatial Graph View" nodeScaleFactor setValue 200
+    "Spatial Graph View" fire
 
     # Take the snapshot
-    viewer 0 snapshot -offscreen 640 480 $base.png
+    viewer 0 setSize 640 480
+    viewer 0 snapshot wholeCellAnalytics/$file.png
 }
 
 
@@ -83,14 +92,13 @@ set files_mrc [glob wholeCellAnalytics/*mitochondrion.mrc]
 foreach file $files_mrc {
     set base [file tail $file]
     set base [string trimright $base ".mrc"]
-    [ load $file ] setLabel $file
-    getvsa $file $base
-    getskl $file $base
-    #saveimg $file $base
+    [ load $file ] setLabel $base
+    getvsa $base
+    getskl $base
+    #saveimg $base
     $base.statistics save "CSV" wholeCellAnalytics/${base}_vsa.csv
     SmoothTree.statistics save "CSV" wholeCellAnalytics/${base}_skl.csv
     $base.remeshed save "HxSurface binary" wholeCellAnalytics/${base}_srf.surf
-    #$base.remeshed save "Open Inventor" ${base}_surf.iv
     SmoothTree.spatialgraph save "AmiraMesh ASCII SpatialGraph" wholeCellAnalytics/${base}_skl.am
     remove -all
 }
